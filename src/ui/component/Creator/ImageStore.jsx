996040612e535595994d0862eea39b3df0ab084c
James Newton <blackslate@lexogram.com>
2023-12-17 18:04:33 +0300

    Generate image store dynamically
    ImageStore.jsx
    ---
    * Add imageMapper() to generate a div.image for the total number of images
    * Show images in the default order, where available
    * Group images by into `<div.one-card>` blocks...
    * ... with the first image always the first in each block
    * Use HARD-CODED colour classes to set the backgroundColor of each one-card div
    * Remove the hand-written divs from div#images-in-cards
    
    Routing.css
    ---
    * Site img width to 100% of div.image
    * Add .purple and .grey background colours

--- src/ui/component/Creator/ImageStore.jsx
+++ src/ui/component/Creator/ImageStore.jsx
@@ -16,81 +16,73 @@ export const ImageStore = () => {
   } = useContext(ImagesContext)
 
 
+  const imageMapper = (_, index) => {
+    const imageData = images[index]
+    if (!imageData) {
+      return (
+        <div
+          key={`empty_${index}`}
+          className='image'
+        />
+      )
+    }
+
+    const { name, size, lastModified } = imageData
+    const trimmedName = name.replace(/\.\w{3,4}$/, "")
+    const src = URL.createObjectURL(imageData)
+
+    return (
+      <div
+        key={`${name}_${size}_${lastModified}`}
+        className='image'
+      >
+        <img src={src} alt={trimmedName} />
+      </div>
+    )
+  }
+
+  const store = Array.from({length: total}, imageMapper)
+  const colours = [
+    "orange",
+    "yellow",
+    "green",
+    "teal",
+    "blue",
+    "royal",
+    "purple",
+    "pink",
+    "grey"
+  ]
+  const firstImage = store.shift()
+
+  const cards = []
+  let ii = 0
+  while (ii < imagesPerCard) { // = number of cards for an image
+    const cardImages = store.splice(0, imagesPerCard-1)
+    cardImages.unshift(firstImage)
+    const colour = colours[ii] || "grey"
+    const className = `one-card ${colour}`
+    const cardDiv = (
+      <div
+        key={`card_${ii}`}
+        className={className}
+      >
+        {cardImages}
+      </div>
+    )
+    cards.push(cardDiv)
+
+    ii++
+  }
+
+
+
   return (
     <div id="image-store">
       <SizeChooser />
 
       <div id="images-in-cards">
-        <div className="one-card orange">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
-        <div className="one-card yellow">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
-        <div className="one-card green">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
-        <div className="one-card teal">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
-        <div className="one-card blue">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
-        <div className="one-card royal">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
-        <div className="one-card pink">
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-          <div className="image"></div>
-        </div>
+        {cards}
       </div>
     </div>
   )
